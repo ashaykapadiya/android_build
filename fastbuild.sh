b867aef3be6d375cb302060fd4a0bfd837ed0f77
MadeOfGreat <ravenklawasd@gmail.com>
2020-07-07 22:55:59 +0530

    Add a script to speed up build

---
+++ fastbuild.sh
@@ -0,0 +1,8 @@
+# This was taken from @AndroidBuildersHelp on telegram, I have no idea who authored it, so please excuse any mistakes regarding that.
+
+mka SystemUI
+mka api-stubs-docs
+mka hiddenapi-lists-docs 
+mka system-api-stubs-docs 
+mka test-api-stubs-docs 
+mka bacon
